6a8dc0e990d2bacda5d36695d5f86e900baa031c
Somnath Gaikwad <somnathdgaikwad@gmail.com>
2021-09-05 14:43:16 +0530

    Intial commit

---
+++ src/main/java/com/test/maven/quickstart/App.java
@@ -0,0 +1,15 @@
+package com.test.maven.quickstart;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       JOptionPane.showConfirmDialog(null, "Hello from maven project");
+    }
+}
